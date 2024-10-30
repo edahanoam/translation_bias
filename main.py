c6736c2a1e8e00e73eb8b64cb3c47387d111f55f
edahanoam1 <90025303+edahanoam1@users.noreply.github.com>
2024-10-30 20:01:14 +0200

    get appropriate cols

--- main.py
+++ main.py
@@ -3,11 +3,26 @@ from datasets import load_dataset
 from huggingface_hub import login
 
 
-if __name__ == '__main__':
-
-
-    ds = load_dataset("FBK-MT/gender-bias-PE", "all")
+def load_data(ambi=False):
+    ds = load_dataset("FBK-MT/gender-bias-PE", "all",split='test')
     print(ds.shape)
+    if not ambi:
+        data = ds.filter(lambda x: x['dataset'] == 'mtgen_un')
+    else:
+        data = ds.filter(lambda x: x['dataset'] == 'mtgen_a')
+    return data
 
-    unambi = ds.filter(lambda x: x['dataset'] == 'mtgen_un')
+def get_suggestions_and_edits(ds):
+    suggestion = ds['suggestion']
+    post_edits = ds['last_translation']
+    return suggestion,post_edits
+
+
+if __name__ == '__main__':
+    data = load_data(False)
+    #print("Columns:", data.column_names)
+
+    suggestion, post_edits = get_suggestions_and_edits(data)
+
+
 
