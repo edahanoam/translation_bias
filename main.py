20ff684a7960565a19fe0389af7128416a724eea
edahanoam1 <90025303+edahanoam1@users.noreply.github.com>
2024-11-02 17:42:00 +0200

    calc difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from datasets import load_dataset
-from huggingface_hub import login
+import evaluate
 
 
 def load_data(ambi=False):
@@ -13,22 +13,41 @@ def load_data(ambi=False):
     return data
 
 
-def get_relevant_cols(ds):
-    original = ds['segment']
-    suggestion = ds['suggestion']
-    post_edits = ds['last_translation']
-    references= ds['tgt']
-    return original,suggestion,post_edits,references
+def get_relevant_cols(ds,Female=True):
+
+    # original = ds['segment']
+    # suggestion = ds['suggestion']
+    # post_edits = ds['last_translation']
+    # references= ds['tgt']
+    if Female:
+        filtered_dataset = ds.filter(lambda row: row['gender'] == 'F')
+    else:
+        filtered_dataset = ds.filter(lambda row: row['gender'] == 'M')
+
+    post_edits = filtered_dataset['last_translation']
+    suggestions = filtered_dataset['suggestion']
+    references = filtered_dataset['tgt']
+    refrences_array = [[s] for s in references]
+
+
+    return suggestions,post_edits,refrences_array
+
+
+def calc_dif(bleu,suggestions_male,references_male,suggestions_female,references_female):
+    male_score=bleu.compute(predictions=suggestions_male, references=references_male)
+    print(male_score)
+
 
 
 if __name__ == '__main__':
     data = load_data(False)
     #print("Columns:", data.column_names)
 
-    original,suggestion, post_edits,references = get_relevant_cols(data)
-    # for sent in original[:10]:
-    #     print(sent)
+    suggestions_female, post_edits_female, references_female = get_relevant_cols(data)
+    suggestions_male, post_edits_male, references_male = get_relevant_cols(data, False)
 
+    bleu = evaluate.load("bleu")
+    calc_dif(bleu,suggestions_male,references_male,suggestions_female,references_female)
 
 
 
