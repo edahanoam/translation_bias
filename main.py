b39ded911c11aa6cfc83725b9e22d46d041ce2a1
edahanoam1 <90025303+edahanoam1@users.noreply.github.com>
2024-11-02 19:34:28 +0200

    calculate all differences

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ def load_data(ambi=False):
     return data
 
 
-def get_relevant_cols(ds,Female=True):
+def get_relevant_cols(ds,Female=True,language=None,prof_level=None):
+    if prof_level:
+        ds = ds.filter(lambda row: row['user_type'] == prof_level)
+    if language:
+        ds = ds.filter(lambda row: row['lang'] == language)
 
     # original = ds['segment']
     # suggestion = ds['suggestion']
@@ -35,31 +39,52 @@ def get_relevant_cols(ds,Female=True):
 
 def calc_dif(bleu,suggestions_male,references_male,suggestions_female,references_female):
     male_score=bleu.compute(predictions=suggestions_male, references=references_male)
-    print(male_score['bleu'])
     female_score=bleu.compute(predictions=suggestions_female, references=references_female)
-    print(female_score['bleu'])
-    print(male_score['bleu']-female_score['bleu'])
+    return male_score['bleu'],female_score['bleu'],male_score['bleu']-female_score['bleu']
 
+def calc_all_options():
+    #abmigous
+    bleu = evaluate.load("bleu")
+    data = load_data(True)
+    results=[]
+    all_options_abmi= [['it','professional'],['es','professional'],['de','professional'],['it','student']]
+    for option in all_options_abmi:
+        suggestions_female, post_edits_female, references_female = get_relevant_cols(data, True, option[0],option[1] )
+        suggestions_male, post_edits_male, references_male = get_relevant_cols(data, False,  option[0],option[1])
+        male,female,dif= calc_dif(bleu,suggestions_male,references_male,suggestions_female,references_female)
+        post_male,post_female, post_dif=calc_dif(bleu,post_edits_male,references_male,post_edits_female,references_female)
+        results.append({'option':option,'male_bleu':male,'female_bleu':female,'dif':dif,'post_male':post_male,'post_female':post_female,'post_dif':post_dif })
+
+    #unabmigous
+    data = load_data(False)
+    suggestions_female, post_edits_female, references_female = get_relevant_cols(data, True)
+    suggestions_male, post_edits_male, references_male = get_relevant_cols(data, False)
+    male, female, dif = calc_dif(bleu, suggestions_male, references_male, suggestions_female, references_female)
+    post_male, post_female, post_dif = calc_dif(bleu, post_edits_male, references_male, post_edits_female,
+                                                references_female)
+
+
+    results.append({'option': ['it','not-ambi'], 'male_bleu': male, 'female_bleu': female, 'dif': dif, 'post_male': post_male,
+                'post_female': post_female, 'post_dif': post_dif})
+
+    df = pd.DataFrame(results)
+    df.to_csv("results.csv")
+    print(results)
 
 
 if __name__ == '__main__':
-    data = load_data(False)
     #print("Columns:", data.column_names)
 
-    suggestions_female, post_edits_female, references_female = get_relevant_cols(data)
-    suggestions_male, post_edits_male, references_male = get_relevant_cols(data, False)
+    calc_all_options()
 
-    bleu = evaluate.load("bleu")
 
-    #calc_dif(bleu,suggestions_male,references_male,suggestions_female,references_female)
 
     #sanity check: reproduce the bleu score in paper
-    suggestions = data['suggestion']
-    references = data['tgt']
-    refrences_array = [[s] for s in references]
-    all_score= bleu.compute(predictions=suggestions, references=refrences_array)
-    print(all_score['bleu'])
-
+    # suggestions = data['suggestion']
+    # references = data['tgt']
+    # refrences_array = [[s] for s in references]
+    # all_score= bleu.compute(predictions=suggestions, references=refrences_array)
+    # print(all_score['bleu'])
 
 
 
