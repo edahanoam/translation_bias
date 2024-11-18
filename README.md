7220264b58f35b5890aa12305fa3b9190e85edc3
Noam Dahan <noam.dahan1@cs.huji.ac.il>
2024-11-18 16:03:01 +0200

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # translation_bias
+In order to run evaluation:
+
+python evaluation.py --in=$DATA --out=$PATH_FOR_OUTPUT
---
+++ evaluation.py
@@ -0,0 +1,18 @@
+""" Usage:
+    <file-name> --in=INPUT_FILE --out=OUTPUT_FILE [--debug]
+
+"""
+
+
+from docopt import docopt
+from pathlib import Path
+
+
+
+if __name__ == '__main__':
+    # Parse command line arguments
+    args = docopt(__doc__)
+    in_file = args["--in"]
+    out_fn = Path(args["--out"])
+    print("you got it")
+
