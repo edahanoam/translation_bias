ecc95b359a86f2ea5f2220901d6053648c1be70b
Noam Dahan <noam.dahan1@cs.huji.ac.il>
2024-11-20 16:44:43 +0200

    all entities on data

--- evaluation.py
+++ evaluation.py
@@ -11,6 +11,8 @@ from datasets import load_dataset
 from huggingface_hub import login
 login()
 
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
 
 
 def load_data(ambi=False):
@@ -23,20 +25,25 @@ def load_data(ambi=False):
     return data
 
 
-def find_entities(text):
-    nlp = spacy.load("en_core_web_sm")
+
+def find_entities_spacy(text, nlp):
     #text = "Barack Obama and Michelle went to the White House."
     doc = nlp(text)
     human_entities = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
+    pronouns = [token.text for token in doc if token.pos_ == "PRON" and token.lower_ in {"he", "she", "they", "him", "her", "them"}]
+    human_entities.extend(pronouns)
+
     #print("Human entities found:", human_entities)
     return human_entities
 
-def find_all_entities(dataset,english_col):
-    #todo: this is very slow consider using something else
-    dataset = dataset.map(
-        lambda row: {"entity": find_entities(row[english_col])},
-        batched=False
-    )
+def find_all_entities(dataset,english_col,model=None):
+    nlp = spacy.load("en_core_web_sm")
+    if not model:
+        dataset = dataset.map(
+            lambda row: {"entity": find_entities_spacy(row[english_col],nlp)},
+            batched=False
+        )
+
     return dataset
 
 
@@ -47,7 +54,9 @@ if __name__ == '__main__':
     in_file = args["--in"]
     out_fn = Path(args["--out"])
     #find_entities()
-    data = load_data(True)
+    data = load_data(False)
     data=find_all_entities(data,english_col="segment")
     print(data['entity'])
+    #
+
 
